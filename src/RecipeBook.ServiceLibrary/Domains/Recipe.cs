8228276d462b3cb8afa93196c601cf06b12c8773
veslorand <61459386+veslorand@users.noreply.github.com>
2021-04-17 01:46:54 +0200

    Organise folder structure

---
+++ src/RecipeBook.ServiceLibrary/Domains/Recipe.cs
@@ -0,0 +1,19 @@
+﻿using RecipeBook.ServiceLibrary.Entities;
+using RecipeBook.ServiceLibrary.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeBook.ServiceLibrary.Domains
+{
+    public class Recipe
+    {
+        public void SaveRecipe(RecipeEntity recipeModel)
+        {
+            var repository = new RecipeRepository();
+            repository.SaveRecipeToDatabase(recipeModel);
+        }
+    }
+}
---
+++ src/RecipeBook.ServiceLibrary/Repositories/RecipeRepository.cs
@@ -0,0 +1,17 @@
+﻿using RecipeBook.ServiceLibrary.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeBook.ServiceLibrary.Repositories
+{
+    public class RecipeRepository
+    {
+        public void SaveRecipeToDatabase(RecipeEntity recipeModel)
+        {
+
+        }
+    }
+}
